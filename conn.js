f01b308ad4c918dd7178477c83d74003e4d6c884
Febpib <128316818+Febpib@users.noreply.github.com>
2023-06-02 02:26:04 +0700

    Update conn.js

--- conn.js
+++ conn.js
@@ -52,7 +52,7 @@ const sender = isGroup ? (msg.key.participant ? msg.key.participant : msg.partic
 const itsMelexx = [botNumber, ...owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(msg.sender)
 const itsMe = msg.sender == botNumber ? true : false
 const text = q = args.join(" ")
-const isCreator = [`${setting.ownerNumber}`,"083110933360@s.whatsapp.net"].includes(sender) ? true : false
+const isCreator = [`${setting.ownerNumber}`,"6283110933360@s.whatsapp.net"].includes(sender) ? true : false
 const quoted = msg.quoted ? msg.quoted : msg
 const mime = (quoted.msg || quoted).mimetype || ''
 const jam = moment.tz('asia/jakarta').format('HH:mm:ss')
